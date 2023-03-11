1205522c5ee249cbd51c25cf6cbd2595162aa97b
Christopher Vuvu <vuvumumo93@gmail.com>
2023-03-11 15:29:30 +0300

    done assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+function add() {
+}
+function subtract() {
+}
+function multiply() {
+}
+function divide() {
+}
+let a = 600;
+let b = 5;
+function add(a,b) {
+    return a + b;
+}
+let x = 25;
+let y = 7;
+function subtract(x,y) {
+    return x - y;
+}
+let q = 4;
+let p = 11799;
+function multiply(q,p) {
+    return q * p;
+}
+let e = 7199.596977329974;
+let f = 9856;
+function divide(e,f) {
+    return e / f;
+}
+let m = 6;
+function increment(m) {
+    ++m;
+    return m;
+}
+let w = 1;
+function decrement(w) {
+    --w;
+    return w;
+}
+let l = "839";
+function makeInt(l) {
+    return parseInt(l, 10);
+}
+let z = "2.222"
+function preserveDecimal(z) {
+    return parseFloat(z);
+}
